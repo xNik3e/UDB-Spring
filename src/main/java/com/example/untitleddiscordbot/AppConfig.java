4d910f1a626995de5820a460c928aad8acb3228e
xNikus <comlokoxd@gmail.com>
2022-11-16 21:38:18 +0100

    Init

---
+++ src/main/java/com/example/untitleddiscordbot/AppConfig.java
@@ -0,0 +1,4 @@
+package com.example.untitleddiscordbot;
+
+public class AppConfig {
+}
---
+++ src/test/java/com/example/untitleddiscordbot/UntitledDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.untitleddiscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
